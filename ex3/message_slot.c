7b80ed69dc9ba7174f1c9f663c2e4da1016c2a0e
Almog Altman <almogaltman1@mail.tau.ac.il>
2022-04-25 10:53:26 +0300

    open and ioctl first version

--- ex3/message_slot.c
+++ ex3/message_slot.c
@@ -26,13 +26,13 @@ typedef struct channel
 typedef struct message_slot_file_info
 {
     int minor; /*is needed???????????????*/
-    unsigned long curr_ch_id;
-    channel *curr_channel;  /*is needed???????????????*/
+    /*unsigned long curr_ch_id;*/ /*is needed???????????????*/
+    channel *curr_channel;  
     channel *head_channel_list;
 } message_slot_file_info;
 
 
-static message_slot_file_info message_devices[256];
+static message_slot_file_info *message_devices[256];
 
 
 /*device functions - all declarations like we saw in recitation*/
@@ -41,10 +41,26 @@ static int device_open(struct inode* inode, struct file* file)
     /*complete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
     message_slot_file_info *ms_info; /*maybe delete??????????*/
     int minor = iminor(inode);
-    printk("Invoking device_open(%p)\n", file);
+    printk("Invoking device_open(%p)\n", file); /*maybe delete??????????*/
+
+
     /*what to do if we allready have this minor?????????????????????????????????
-    is this ok to not do anithing???*/
-   if (message_devices[minor] == NULL)
+    is this ok to not do anything???*/
+    if (message_devices[minor] == NULL)
+    {
+        ms_info = kalloc(sizeof(message_slot_file_info), GFP_KERNEL);
+        ms_info->curr_channel = NULL;
+        ms_info->head_channel_list = NULL;
+        ms_info->minor = minor;
+
+        file->private_data = (void *)ms_info;
+        message_devices[minor] = ms_info; /*like this ?????????????????????????*/    
+    }
+    else
+    {
+        /*is this ok????????????????*/
+        file->private_data = (void *)message_devices[minor];
+    }
 
     return SUCCESS;
 }
@@ -53,6 +69,10 @@ static int device_open(struct inode* inode, struct file* file)
 static int device_release(struct inode* inode, struct file* file)
 {
     /*complete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+    /*is really here?????*/
+
+    /*free all device channels*/
+
     return SUCCESS;
 }
 
@@ -76,16 +96,41 @@ static ssize_t device_write(struct file* file, const char __user* buffer, size_t
 
 static long device_ioctl(struct file* file, unsigned int ioctl_command_id, unsigned long  ioctl_param)
 {
-    /*
-    // Switch according to the ioctl called
-    if( IOCTL_SET_ENC == ioctl_command_id )
+    message_slot_file_info *ms_info;
+    channel *ch;
+
+    if (ioctl_command_id != MSG_SLOT_CHANNEL || ioctl_param == 0)
     {
-        // Get the parameter given to ioctl by the process
-        printk( "Invoking ioctl: setting encryption "
-                "flag to %ld\n", ioctl_param );
-        encryption_flag = ioctl_param;
+        return -EINVAL;
     }
-    */
+
+    printk("Invoking ioctl: setting channel id to %ld\n", ioctl_param);
+    ms_info = (message_slot_file_info *)file->private_data;
+    ch = ms_info->head_channel_list;
+
+    if (ch != NULL)
+    {
+        /*need to find the channel*/
+        while (ch != NULL)
+        {
+            if (ch->ch_id == ioctl_param)
+            {
+                /*this is the rellevant channel*/
+                ms_info->curr_channel = ch;
+                file->private_data = (void *)ms_info; /*is that needed??????????????*/
+                return SUCCESS;
+            }
+            ch = ch->next;
+        }
+    }
+    
+    /*this is the first channel we make or we dowsent have this channel*/
+    ch = kalloc(sizeof(channel), GFP_KERNEL);
+    ch->ch_id = ioctl_param;
+    ch->curr_message_size = 0;
+    ch->next = NULL;
+    ms_info->curr_channel = ch;
+    file->private_data = (void *)ms_info; /*is that needed??????????????*/
     return SUCCESS;
 }
 
